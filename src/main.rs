41a5e3c887da19513bd793fa92e23f98c9d3ff5e
mindcool24 <thezachm24@gmail.com>
2025-03-19 16:57:40 -0600

    It works I guess, there is some data in the db under the table energyprices

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use std::path::Path;
 use std::fs::File;
 use std::io::Write;
 use std::io::BufReader;
+use chrono::NaiveDateTime;
 use chrono::{DateTime, Local};
-
 use xml::reader::{EventReader, XmlEvent};
+use dotenv;
+use mysql::*;
+use mysql::prelude::*;
+use std::time;
 
 
 #[derive(Debug)]
@@ -12,15 +16,21 @@ struct PriceEntry {
     data_item: String,
     resource_name: String,
     interval_num: i32,
-    interval_start: DateTime::<Local>,
-    interval_end: DateTime::<Local>,
+    interval_start: NaiveDateTime,
+    interval_end: NaiveDateTime,
     price: f32,
 }
 
 
 #[tokio::main]
 async fn main() {
+    let db_pass = dotenv::var("DB_PASS").unwrap();
+    let db_user = dotenv::var("DB_USER").unwrap();
+    let db_name = dotenv::var("DB_NAME").unwrap();
+    let db_host = dotenv::var("DB_HOST").unwrap();
+
     let curr: DateTime<Local> = Local::now();
+    let mut data_points: Vec<PriceEntry> = Vec::new();
 
     let formatted = format!("{}", curr.format("http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_INTVL_LMP&startdatetime=%Y%m%dT00:00-0600&enddatetime=%Y%m%dT23:00-0600&version=3&market_run_id=RTM&node=LOGANCIT_LNODED1"));
     let resp = reqwest::get(formatted).await.unwrap();
@@ -82,10 +92,10 @@ async fn main() {
                     parser.next();
 
                     // Start interval time
-                    let start_intvl: DateTime<Local>;
+                    let start_intvl: NaiveDateTime;
                     match parser.next() {
                         Ok(XmlEvent::Characters(val)) => {
-                            start_intvl = DateTime::parse_from_str(&val, "%Y-%m-%dT%T%:z").unwrap().to_utc().naive_local().and_local_timezone(Local).unwrap();
+                            start_intvl = DateTime::parse_from_str(&val, "%Y-%m-%dT%T%:z").unwrap().to_utc().naive_local().and_local_timezone(Local).unwrap().naive_local();
                         }
                         _ => {
                             return;
@@ -96,10 +106,10 @@ async fn main() {
                     parser.next();
 
                     // End interval time
-                    let mut end_intvl: DateTime<Local>;
+                    let mut end_intvl: NaiveDateTime;
                     match parser.next() {
                         Ok(XmlEvent::Characters(val)) => {
-                            end_intvl = DateTime::parse_from_str(&val, "%Y-%m-%dT%T%:z").unwrap().to_utc().naive_local().and_local_timezone(Local).unwrap();
+                            end_intvl = DateTime::parse_from_str(&val, "%Y-%m-%dT%T%:z").unwrap().to_utc().naive_local().and_local_timezone(Local).unwrap().naive_local();
                         }
                         _ => {
                             return;
@@ -118,7 +128,7 @@ async fn main() {
                         _ => {}
                     }
 
-                    let thingy = PriceEntry{
+                    let point = PriceEntry{
                         data_item: prc_item,
                         resource_name: r_name,
                         interval_num: itvl_num,
@@ -126,6 +136,7 @@ async fn main() {
                         interval_end: end_intvl,
                         price: value
                     };
+                    data_points.push(point);
                 }
             }
             Ok(XmlEvent::EndDocument) => {
@@ -134,4 +145,32 @@ async fn main() {
             _ => {}
         }
     }
+    let url = Opts::from_url(format!("mysql://{}:{}@{}:3306/{}", db_user, db_pass, db_host, db_name).as_str()).unwrap();
+    let pool = Pool::new(url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+
+    conn.query_drop(r"CREATE TABLE IF NOT EXISTS energyprices (
+            data_item text not null,
+            resource_name text not null,
+            interval_num int not null,
+            interval_start datetime not null,
+            interval_end datetime not null,
+            price float not null
+        )"
+    ).unwrap();
+
+    conn.exec_batch(
+        r"INSERT INTO energyprices (data_item, resource_name, interval_num, interval_start, interval_end, price)
+        VALUES (:data_item, :resource_name, :interval_num, :interval_start, :interval_end, :price)",
+        data_points.iter().map(|p| params!{
+            "data_item" => p.data_item.clone(),
+            "resource_name" => p.resource_name.clone(),
+            "interval_num" => p.interval_num,
+            "interval_start" => p.interval_start,
+            "interval_end" => p.interval_end,
+            "price" => p.price
+        })
+    ).unwrap();
+
+
 }
