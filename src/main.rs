0d5458363ea99b8109bd7d89bab3d95259762f33
mindcool24 <thezachm24@gmail.com>
2025-03-19 12:05:01 -0600

    Now pulls from current date

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::os::unix::process;
 use std::path::Path;
 use std::fs::File;
 use std::io::Write;
 use std::io::BufReader;
+use chrono::{DateTime, Local};
 
 use xml::reader::{EventReader, XmlEvent};
 
@@ -11,16 +11,19 @@ use xml::reader::{EventReader, XmlEvent};
 struct PriceEntry {
     data_item: String,
     resource_name: String,
-    //interval_num: i32,
+    interval_num: i32,
     //interval_start: String, // Change this later to be a timestamp
     //interval_end: String,   // Same here
-    price: String //f32,
+    price: f32,
 }
 
 
 #[tokio::main]
 async fn main() {
-    let resp = reqwest::get("http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_INTVL_LMP&startdatetime=20250102T07:00-0000&enddatetime=20250102T08:00-0000&version=3&market_run_id=RTM&node=LOGANCIT_LNODED1").await.unwrap();
+    let curr: DateTime<Local> = Local::now();
+
+    let formatted = format!("{}", curr.format("http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_INTVL_LMP&startdatetime=%Y%m%dT00:00-0000&enddatetime=%Y%m%dT23:00-0000&version=3&market_run_id=RTM&node=LOGANCIT_LNODED1"));
+    let resp = reqwest::get(formatted).await.unwrap();
 
     let path = Path::new("./data.zip");
     let path1 = Path::new("./data/");
@@ -42,7 +45,7 @@ async fn main() {
     let mut processing = true;
 
     while processing {
-        let mut element = parser.next();
+        let element = parser.next();
         match element {
             Ok(XmlEvent::Characters(data)) => {
                 if data == "LMP_PRC" {
@@ -64,10 +67,10 @@ async fn main() {
                     parser.next();
                     parser.next();
                     parser.next();
-                    let mut itvl_num = "".to_string();
+                    let mut itvl_num = 0;
                     match parser.next() {
                         Ok(XmlEvent::Characters(val)) => {
-                            itvl_num = val;
+                            itvl_num = val.parse::<i32>().expect("Bad interval number");
                         }
                         _ => {}
                     }
@@ -82,10 +85,10 @@ async fn main() {
                     parser.next();
                     parser.next();
                     parser.next();
-                    let mut value = "".to_string();
+                    let mut value = 0.0;
                     match parser.next() {
                         Ok(XmlEvent::Characters(val)) => {
-                            value = val;
+                            value = val.parse::<f32>().expect("Bad data, price was not float");
                         }
                         _ => {}
                     }
@@ -93,38 +96,17 @@ async fn main() {
                     let thingy = PriceEntry{
                         data_item: prc_item,
                         resource_name: r_name,
+                        interval_num: itvl_num,
                         price: value
                     };
                     println!("{:?}", thingy);
                 }
             }
+            Ok(XmlEvent::EndDocument) => {
+                processing = false;
+            }
+        
             _ => {}
         }
     }
-
-    /*
-    for i in parser {
-        match i {
-            Ok(XmlEvent::StartElement { name, ..}) => {
-                println!("{:spaces$}-{n}", "", spaces = depth * 2, n = name.local_name);
-                depth += 1
-            }
-            Ok(XmlEvent::EndElement { name }) => {
-                depth -= 1;
-                println!("{:spaces$}-{n}", "", spaces = depth * 2, n = name.local_name);
-            }
-            Ok(XmlEvent::Characters(data)) => {
-                if data == "LMP_PRC" {
-                    println!("Hey look thingy aaaaaaaaaaaaa")
-                }
-            }
-            Err(i) => {
-                eprintln!("Error: {i}");
-                break;
-            }
-            _ => {}
-        }
-    }
-    */
-
 }
